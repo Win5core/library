41d0bdcf167113fec6f14f205a15269ad0c2fc92
Win5core <97760551+Win5core@users.noreply.github.com>
2022-12-09 18:28:32 +0330

    yes

--- library.py
+++ library.py
@@ -1,1 +1,27 @@
-i = input("Welcome to my library program!\n1 Overview\n2 Borrow a book\n3 Return a book\nEnter the number to continue")
+import os
+
+# Function to easily clear history
+def clear():
+    os.system('cls')
+
+# Gets running python directory
+path = os.getcwd()
+# The following shows how to read all texts from the readme.txt file into a string
+# If it doesn't find the file (or maybe directory),
+try:
+    with open(path) as f:
+        lines = f.readlines()
+except FileNotFoundError:
+    open(path, 'w')
+
+clear()
+
+print("Welcome to my library program!\n1 Overview\n2 Manage Books\n3 ?\n4 Manage members")
+
+
+# NEXT CHANGE: Checks if data file is ok and tells the user
+
+
+# Select an option
+i = input("\n\nEnter a number to continue: ")
+clear()
