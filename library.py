af3760e05a05c9fd7d96dee6d9f00da673c0d3a1
Win5core <97760551+Win5core@users.noreply.github.com>
2022-12-12 23:10:15 +0330

    -Changed function clear to cls -Changed getcwd to path.dirname, since it doesnt get the path we wanted -Worked on data reader

--- library.py
+++ library.py
@@ -1,28 +1,37 @@
 import os
 
-def clear():
+def cls():
     os.system('cls')
 
-path = os.getcwd()
+path = os.path.dirname(__file__)
 
-# path = (os.getcwd() + "\\file.txt")
+people = (path + "\data\people.txt")
 
-clear()
+cls()
 
-# phrase.replase("Giraffe", "Elephant")
+# phrase.replace("Giraffe", "Elephant")
 
-"""
 try:
-    with open('path') as f:
+    with open('path' , 'r') as f:
         lines = f.readlines()
 except FileNotFoundError:
-    print("")
-"""
-i = "x"
-while i == 0:
-    print(" Welcome to my library program!\n1 Overview\n2 Manage Books\n3 Manage members")
+    lines = False
+
+
+
+print("Welcome to my library program!\n1 Overview\n2 Manage Books\n3 Manage members\n0 Exit")
+
 
 # NEXT CHANGE: Checks if data file is ok and tells the user
 
+
 i = input("\n\nEnter a number to continue: ")
-clear()
+
+while int(i) != 0:
+    if int(i) == 1:
+        print("Overview")
+    if int(i) == 2:
+        pass
+
+
+
