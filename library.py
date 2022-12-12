06ccbd3f3ab018b4c27d5f1cc6ac36d8c659cf5b
Win5core <97760551+Win5core@users.noreply.github.com>
2022-12-13 00:39:55 +0330

    - Edited "add book/person to list" - Worked on data editor

--- library.py
+++ library.py
@@ -2,29 +2,53 @@ import os
 
 def cls():
     os.system('cls')
-
-path = os.path.dirname(__file__)
-
-people = (path + "\data\people.txt")
-
 cls()
 
-# phrase.replace("Giraffe", "Elephant")
+memberspath = ((os.path.dirname(__file__)) + "\data\members.txt")
+bookspath = ((os.path.dirname(__file__)) + "\data\\books.txt")
 
+# memberspath
 try:
     with open('path' , 'r') as f:
-        lines = f.readlines()
+        memberspath = f.readlines()
 except FileNotFoundError:
-    lines = False
+    print("Data for not found, made a new data file")
+    open(memberspath , 'w')
+# bookspath
+try:
+    with open('path' , 'r') as f:
+        bookspath = f.readlines()
+except FileNotFoundError:
+    print("Data for books not found, made a new data file")
+    open(bookspath , 'w')
+# phrase.replace("Giraffe", "Elephant")
+
+
+# ................................................................................................................................
+
+# add book/person to list
+"""
+i = 1
+bookspath = []
+while i != 0:
+    x = input("\n")
+    if x == "0":
+        i = 0
+    else:
+        bookspath.append(x)
+        print(bookspath)
+"""
+
+
+
+
+
+
+
 
 
 
 print("Welcome to my library program!\n1 Overview\n2 Manage Books\n3 Manage members\n0 Exit")
-
-
-# NEXT CHANGE: Checks if data file is ok and tells the user
-
-
 i = input("\n\nEnter a number to continue: ")
 
 while int(i) != 0:
