83f3fedd08a9329b7e94411f6a0022c92e5e4006
Win5core <97760551+Win5core@users.noreply.github.com>
2022-12-23 14:03:43 +0330

    nth

--- library.py
+++ library.py
@@ -1,75 +1,80 @@
+# Making a short command to use cls
 import os
-
 def cls():
     os.system('cls')
 cls()
+
+# Importing paths as a string to variable
 memberspath = ((os.path.dirname(__file__)) + "\data\members.txt")
 bookspath = ((os.path.dirname(__file__)) + "\data\\books.txt")
 path = ((os.path.dirname(__file__)) + "\data")
 
-# memberspath
-try:
-    print("Creating path...")
+# Data file checker
+if os.path.exists(path) == False:
     os.mkdir(path)
-except FileExistsError:
-    print("Path already exists!")
-try:
-    with open(memberspath , 'r') as f:
-        members = f.readlines
-        print("wut")
-except FileNotFoundError:
-    print("Members data not found, made a new data file")
-    open(memberspath , 'w')
-        
-# bookspath , needs edit
-"""
-try:
-    print("Creating path...")
-    os.mkdir(path)
-except FileExistsError:
-    print("Path already exists!")
-try:
-    with open(memberspath , 'r') as f:
-        members = f.readlines
-        print("wut")
-except FileNotFoundError:
-    print("Members data not found, made a new data file")
-    open(memberspath , 'w')
-"""
+if os.path.isfile(memberspath) == False:
+        open(memberspath , 'w')
+if os.path.isfile(bookspath) == False:
+        open(bookspath , 'w')
 
-# ................................................................................................................................
+def menu0():
+    cls()
+    print("Welcome to my library program!\n1 Overview\n2 Manage Books\n3 Manage members\n0 Exit")
+def menu3():
+    cls()
+    print("1 View list of members\n2 Add a new member\n3 Remove a member\n0 Return to previous menu")
+i0 = ()
+i1 = ()
+i2 = ()
+i3 = ()
 
-# add book/person to list
-"""
-i = 1
-books = []
-while i != 0:
-    x = input("\n")
-    if x == "0":
-        i = 0
+while i0 != 0:
+    menu0()
+    i0 = int(input("\nEnter a number: "))
+    if i0 == 1:
+        # Menu1
+        print("option 1")
+    elif i0 == 2:
+        # Menu2
+        print("option 2")
+    elif i0 == 3:
+        # Menu3
+        while i3 !=0:
+            menu3()
+            try:
+                i3 = int(input("\nEnter a number: "))
+            except ValueError:
+                pass
+            if i3 == 1:
+                cls()
+                with open(memberspath) as file: 
+                    while (line := file.readline().rstrip()): 
+                        print(line)
+                input("\nPress enter to continue")
+            elif i3 == 2:
+                    cls()
+                    members = []
+                    with open(memberspath) as file: 
+                        for line in file: 
+                            members.append(line.rstrip())
+                    newmember = (str(input("Enter the name: ")))
+                    members.append(newmember)
+                    file = open(memberspath,'w')
+                    for member in members:
+                        file.write(member + "\n")
+                    file.close()
+                    input("Done! Press enter to continue")
+            elif i3 == 3:
+                pass
+            elif i3 == 0:
+                i0 = ()
+                pass
+            else:
+                cls()
+                menu3()
+                i2 = int(input("\nInvalid number! Enter again: "))
+    elif i0 == 0:
+        cls
     else:
-        bookspath.append(x)
-        print(bookspath)
-"""
-
-
-
-
-
-
-
-
-
-
-print("Welcome to my library program!\n1 Overview\n2 Manage Books\n3 Manage members\n0 Exit")
-i = input("\n\nEnter a number to continue: ")
-
-while int(i) != 0:
-    if int(i) == 1:
-        # print("Overview")
-        pass
-    if int(i) == 2:
-        pass
-cls()
-
-
+        menu0()
+        i0 = int(input("\nInvalid number! Enter again: "))
